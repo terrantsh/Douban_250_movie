a50a968453c58366577d530bdab5a01351de6522
terrantsh <tanshanhe@foxmail.com>
2020-05-17 18:02:08 +0800

    添加主工程

---
+++ spider.py
@@ -0,0 +1,17 @@
+#-*- coding = utf-8 -*-
+#@Time      : 2020/5/17 16:49
+#@Author    : Shanhe.Tan
+#@File      : spider.py
+#@Software  : PyCharm
+
+import sys
+import re
+import bs4
+
+
+def main():
+    print("hello")
+
+if __name__ == "__main__":  #当程序执行时
+    #调用函数
+    main()
