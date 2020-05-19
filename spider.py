efdebf2a75bb4acf105ce786bc189da7d5d3582e
terrantsh <tanshanhe@foxmail.com>
2020-05-19 22:07:36 +0800

    添加爬取网页代码

--- spider.py
+++ spider.py
@@ -18,12 +18,20 @@ def main():
     datalist = getData(baseurl)
     savepath = ".\\豆瓣电影Top250.xls"
     #保存数据
-    savaData(savepath)
+    # savaData(savepath)
+
+    askURL("https://movie.douban.com/top250?start=0")
 
 #爬取网页
 def getData(baseurl):
     datalist = []
-    # 逐一解析数据
+    for i in range(0,10):           #调用获取页面信息的函数，10次
+        url = baseurl + str(i * 25)
+        html = askURL(url)          #保存获取到的网页源码
+
+        # 逐一解析数据
+
+
     return datalist
 
 #得到指定一个URL的网页内容
@@ -35,8 +43,15 @@ def askURL(url):
     request = urllib.request.Request(url, headers=head)
     html = ""
     try:
-        response = urllib.request.urlopen()
-    except:
+        response = urllib.request.urlopen(request)
+        html = response.read().decode("utf-8")
+        print(html)
+    except urllib.error.URLError as e:
+        if hasattr(e, "code"):
+            print(e.code)
+        if hasattr(e, "reason"):
+            print(e.reason)
+    return html
 
 
 
---
+++ test/testBs4.py
@@ -0,0 +1,20 @@
+#-*- coding = utf-8 -*-
+#@Time      : 2020/5/19 20:52
+#@Author    : Shanhe.Tan
+#@File      : testBs4.py
+#@Software  : PyCharm
+
+
+'''
+TAG
+NavigableString
+BeautifulSoup
+Comment
+'''
+
+from bs4 import BeautifulSoup
+
+#1、Tag  标签及其内容：拿到他所找到的第一个内容
+#2、NavigableString  标签里面的内容（字符串）
+#3、BeautifulSoup    表示整个文档
+#4、Comment  是一个特殊的NavigableString，但是不包含注释符号
